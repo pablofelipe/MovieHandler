c74a6380f2b3e4a5484e6505890ab9698d105b7b
pablofelipe <pablofelipe@gmail.com>
2023-06-04 18:51:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MovieHandler.Controller;
+using System;
+
+namespace MovieHandler
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You need to use at least one input parameter with the url of the movie database website");
+                return;
+            }
+
+            new MovieHandlerApp(args[0]).Execute();
+        }
+    }
+}
